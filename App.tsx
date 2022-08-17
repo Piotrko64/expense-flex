078906922f37908d4a541816b61874337db9a047
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-08-17 20:10:55 +0200

    add 'add button' on header

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { ManageExpense } from './screens/ManageExpense';
-import { RecentExpenses } from './screens/RecentExpenses';
-import { AllExpenses } from './screens/AllExpenses';
-import { GlobalColors } from './constants/styles';
-import { Ionicons } from '@expo/vector-icons';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { ManageExpense } from "./screens/ManageExpense";
+import { RecentExpenses } from "./screens/RecentExpenses";
+import { AllExpenses } from "./screens/AllExpenses";
+import { GlobalColors } from "./constants/styles";
+import { Ionicons } from "@expo/vector-icons";
+import { IconButton } from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
@@ -18,17 +19,28 @@ function ExpensesOverview() {
                 headerStyle: {
                     backgroundColor: GlobalColors.primary500,
                 },
-                headerTintColor: 'white',
+                headerTintColor: "white",
                 tabBarStyle: { backgroundColor: GlobalColors.primary500 },
                 tabBarActiveTintColor: GlobalColors.accent500,
+                tabBarInactiveTintColor: GlobalColors.primary100,
+                headerRight: ({ tintColor }) => {
+                    return (
+                        <IconButton
+                            icon="add"
+                            size={28}
+                            color={tintColor}
+                            onPress={() => {}}
+                        />
+                    );
+                },
             }}
         >
             <BottomTabs.Screen
                 name="RecentExpenses"
                 component={RecentExpenses}
                 options={{
-                    title: 'Recent Expenses',
-                    tabBarLabel: 'Recent',
+                    title: "Recent Expenses",
+                    tabBarLabel: "Recent",
                     tabBarIcon: ({ color, size }) => (
                         <Ionicons name="hourglass" size={size} color={color} />
                     ),
@@ -38,8 +50,8 @@ function ExpensesOverview() {
                 name="AllExpenses"
                 component={AllExpenses}
                 options={{
-                    title: 'All Expenses',
-                    tabBarLabel: 'All',
+                    title: "All Expenses",
+                    tabBarLabel: "All",
                     tabBarIcon: ({ color, size }) => (
                         <Ionicons name="calendar" size={size} color={color} />
                     ),
@@ -52,7 +64,7 @@ function ExpensesOverview() {
 export default function App() {
     return (
         <>
-            <StatusBar style="auto" />
+            <StatusBar style="light" />
             <NavigationContainer>
                 <Stack.Navigator>
                     <Stack.Screen
