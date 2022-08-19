aab6f2f00014c282ee9087e669d7dc31d607bb9d
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-08-19 21:21:15 +0200

    fix problem with async storage

--- hooks/useUpdateAsyncStorage.tsx
+++ hooks/useUpdateAsyncStorage.tsx
@@ -1,53 +1,22 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { expenseExample } from "../data/dummyData/expensesExample";
-import { updateExpense, updateAll } from "../store/expenses";
+
+import { updateAll } from "../store/expenses";
 
 export async function useUpdateAsyncStorage() {
-    const [isStartUpdate, setIsStartUpdate] = useState(false);
-
     const stateExpenses = useSelector((state: any) => state.expensesReducer);
     const dispatch = useDispatch();
-
-    // async function saveToLocalStorage() {
-    //     const asyncStorage = await AsyncStorage.getItem("@storage_expense");
-    //     if (asyncStorage) {
-    //         try {
-    //             await AsyncStorage.setItem(
-    //                 "@storage_expense",
-    //                 JSON.stringify(stateExpenses)
-    //             );
-    //         } catch (error) {
-    //             console.log(error);
-    //         }
-    //     }
-    // }
-
-    // async function updateReducer() {
-    //     const asyncStorage = await AsyncStorage.getItem("@storage_expense");
-
-    //     dispatch(
-    //         updateExpense(
-    //             asyncStorage ? JSON.parse(asyncStorage) : expenseExample
-    //         )
-    //     );
-
-    // }
 
     async function updateStateFromStorage() {
         const asyncStorage = await AsyncStorage.getItem("@storage_expense");
         console.log(asyncStorage);
         if (asyncStorage) {
             dispatch(updateAll(JSON.parse(asyncStorage)));
-            console.log("init");
-            setIsStartUpdate(true);
         }
     }
 
     async function updateStorage() {
-        const asyncStorage = await AsyncStorage.getItem("@storage_expense");
-
         try {
             await AsyncStorage.setItem(
                 "@storage_expense",
@@ -56,8 +25,6 @@ export async function useUpdateAsyncStorage() {
         } catch (error) {
             console.log(error);
         }
-
-        console.log("storage", asyncStorage);
     }
 
     useEffect(() => {
