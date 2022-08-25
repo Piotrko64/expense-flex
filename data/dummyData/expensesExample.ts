fa0be45854e5263f0b2a138d3820cf4edde5fd1b
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-08-25 07:55:20 +0200

    change example Expense

--- data/dummyData/expensesExample.ts
+++ data/dummyData/expensesExample.ts
@@ -2,12 +2,6 @@ import { OneExpense } from "../../@types/OneExpense";
 
 export const expenseExample: Array<OneExpense> = [
     {
-        id: "el",
-        description: "Don't worry",
-        amount: "0",
-        date: "2022-08-17",
-    },
-    {
         id: "el1",
         description: "Be happy",
         amount: "0",
