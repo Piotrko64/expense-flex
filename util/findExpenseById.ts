53b6915cc33686c98e78eee8e751c2e2f329dd58
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-08-25 19:41:55 +0200

    Preparation for first tests

--- util/findExpenseById.ts
+++ util/findExpenseById.ts
@@ -1,7 +1,8 @@
-export function findExpenseById(list: Array<any>, id: string) {
+import { OneExpense } from "./../@types/OneExpense";
+export function findExpenseById(list: Array<OneExpense>, id: string) {
     return list.find((elementList) => elementList.id === id);
 }
 
-export function findIndexExpense(list: Array<any>, id: string) {
+export function findIndexExpense(list: Array<OneExpense>, id: string) {
     return list.findIndex((elementList) => elementList.id === id);
 }
